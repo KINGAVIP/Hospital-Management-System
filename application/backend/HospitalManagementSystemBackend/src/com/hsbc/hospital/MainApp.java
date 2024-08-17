01b6932b50cd8dc1b5a3e704c4d34d21cfe08023
KINGAVIP <avipruthi8@gmail.com>
2024-08-17 12:33:52 +0530

    Backend Project Structure and Setup

---
+++ application/backend/HospitalManagementSystemBackend/src/com/hsbc/hospital/MainApp.java
@@ -0,0 +1,7 @@
+package com.hsbc.hospital;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Hospital Management System");
+    }
+}
---
+++ application/backend/HospitalManagementSystemBackend/src/com/hsbc/hospital/model/User.java
@@ -0,0 +1,4 @@
+package com.hsbc.hospital.model;
+
+public class User {
+}
