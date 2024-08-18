a7b00f250b7b6a715e081e62baf6ecf777bd4614
sohith-cyber <velagasohith5196@gmail.com>
2024-08-18 21:00:40 +0530

    UpdatedUserService

--- application/backend/hospitalmanagementsystembackend/src/main/java/com/hsbc/hospitalmanagement/domain/User.java
+++ application/backend/hospitalmanagementsystembackend/src/main/java/com/hsbc/hospitalmanagement/domain/User.java
@@ -1,11 +1,17 @@
 package com.hsbc.hospitalmanagement.domain;
 
 public class User extends Profile{
+    private String username;
+    private String password;
 
     final String role = "User";
+    public User() {
 
-    public User(String id, String name, String phoneNumber) {
+    }
+    public User(String id, String name, String phoneNumber, String username, String password) {
         super(id, name, phoneNumber);
+        this.username = username;
+        this.password = password;
     }
 
     public User(String id, String name, String phoneNumber, String address) {
@@ -15,4 +21,21 @@ public class User extends Profile{
     public String getRole() {
         return role;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
