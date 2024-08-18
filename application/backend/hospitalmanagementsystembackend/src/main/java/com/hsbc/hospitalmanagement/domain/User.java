fd3c4e4f3edadc80ae92a471448809625a285656
tinachoudary1762 <tinachoudary1762@gmail.com>
2024-08-18 20:09:57 +0530

    DoctorService DoctorServiceImpl added

--- application/backend/hospitalmanagementsystembackend/src/main/java/com/hsbc/hospitalmanagement/domain/User.java
+++ application/backend/hospitalmanagementsystembackend/src/main/java/com/hsbc/hospitalmanagement/domain/User.java
@@ -1,18 +1,60 @@
 package com.hsbc.hospitalmanagement.domain;
 
-public class User extends Profile{
+import java.util.Objects;
 
-    final String role = "User";
+public class User extends Profile {
 
-    public User(String id, String name, String phoneNumber) {
+    private static final String ROLE = "User";
+    private String id;
+    private String username;
+    private String password;
+
+    // Constructor
+    public User(String id, String name, String phoneNumber, String username, String password) {
         super(id, name, phoneNumber);
+        this.username = username;
+        this.password = password;
     }
 
-    public User(String id, String name, String phoneNumber, String address) {
-        super(id, name, phoneNumber, address);
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getRole() {
-        return role;
+        return ROLE;  // Return the static final constant
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
